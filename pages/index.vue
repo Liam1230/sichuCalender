6b61f44162ea15e2c2eddfc3e50b9ffc71483101
Liam1230 <Kin0k0daichi>
2019-12-12 16:28:16 +0900

    卓上型に対応,少しレイアウト修正

--- pages/index.vue
+++ pages/index.vue
@@ -16,14 +16,14 @@
 				</v-card-text>
 				<v-date-picker v-model="picker" type="month" color="cyan lighten-1" locale="jp-ja" :day-format="date => new Date(date).getDate()"></v-date-picker>
 				<v-card-actions>
-					<v-btn v-on:click="showPreview()">プレビュー</v-btn>
-					<v-btn v-on:click="downloadCanvas()">ダウンロード</v-btn>
-					<v-btn v-on:click="downloadYearCanvas()">一年分ダウンロード</v-btn>
+					<v-btn v-on:click="downloadYearCanvas(0)">一年分ダウンロード(手帳型)</v-btn>
+					<v-btn v-on:click="downloadYearCanvas(1)">一年分ダウンロード(卓上型)</v-btn>
 				</v-card-actions>
 			</v-card>
 			<v-card dark>
 				<v-layout class="justify-center my-5">
 					<canvas id="preview" class="my-5" width="1748" height="2480"></canvas>
+					<canvas id="desk" class="my-5" width="2125" height="1523"></canvas>
 				</v-layout>
 			</v-card>
 		</v-flex>
@@ -53,28 +53,30 @@ class Vector2{
 	}
 }
 
-const canvasWidth = 1748
-const canvasHeight = 2480
+const canvasWidth = [1748,2125]
+const canvasHeight = [2480,1523]
 const scale = 3;
-const YEAR_POS = new Vector2(245 * scale,120 * scale);
-const MONTH_EN_POS = new Vector2(290 * scale,147 * scale);
-const MONTH_POS = new Vector2(265 * scale,90 * scale);
+const YEAR_POS = [new Vector2(254 * scale,120 * scale), new Vector2(318 * scale,120 * scale)];
+const MONTH_EN_POS = [new Vector2(280 * scale,147 * scale), new Vector2(338 * scale,147 * scale)];
+const MONTH_POS = [new Vector2(265 * scale,90 * scale), new Vector2(325 * scale,90 * scale)];
+const MONTH_EN_OFFSET = [5, 4]
+const MONTH_OFFSET = [30, 20]
 
-const PIC_POS = new Vector2(10 * scale,10 * scale);
-const HYOU_POS = new Vector2(360 * scale,35 * scale);
+const PIC_POS = [new Vector2(10 * scale,10 * scale),new Vector2(10 * scale,10 * scale)];
+const HYOU_POS = [new Vector2(360 * scale,35 * scale),new Vector2(470 * scale,35 * scale)];
 
-const WEEKFRAME_OFFSET = new Vector2(0,-30 * scale)
+const WEEKFRAME_OFFSET = [new Vector2(0,-30 * scale),new Vector2(0,-30 * scale)]
 
-const FRAME_LU = new Vector2(30 * scale,200 * scale);
-const FRAME_LD = new Vector2(30 * scale,800 * scale);
-const FRAME_RD = new Vector2(550 * scale,800 * scale);
-const FRAME_RU = new Vector2(550 * scale,200 * scale);
+const FRAME_LU = [new Vector2(30 * scale,200 * scale), new Vector2(30 * scale,200 * scale)];
+const FRAME_LD = [new Vector2(30 * scale,800 * scale), new Vector2(30 * scale,500 * scale)];
+const FRAME_RD = [new Vector2(550 * scale,800 * scale),new Vector2(678 * scale,500 * scale)];
+const FRAME_RU = [new Vector2(550 * scale,200 * scale), new Vector2(678 * scale,200 * scale)];
 
-const WEEK_OFFSET = new Vector2(12 * scale,-7 * scale);
-const DAY_OFFSET = new Vector2(5 * scale,25 * scale);
-const KANSHI_OFFSET = new Vector2(5 * scale,55 * scale);
-const UNSEI_OFFSET = new Vector2(33 * scale,20 * scale);
-const ICON_OFFSET = new Vector2(40 * scale,35 * scale);
+const WEEK_OFFSET = [new Vector2(12 * scale,-7 * scale),new Vector2(12 * scale,-7 * scale)];
+const DAY_OFFSET = [new Vector2(5 * scale,25 * scale),new Vector2(5 * scale,25 * scale)];
+const KANSHI_OFFSET = [new Vector2(5 * scale,55 * scale),new Vector2(5 * scale,45 * scale)];
+const UNSEI_OFFSET = [new Vector2(33 * scale,20 * scale),new Vector2(33 * scale,20 * scale)];
+const ICON_OFFSET = [new Vector2(40 * scale,35 * scale),new Vector2(40 * scale,22 * scale)];
 
 const ROKUJU_KANSHI = [
 		'甲子','乙丑','丙寅','丁卯','戊辰','己巳','庚午','辛未','壬申','癸酉','甲戌','乙亥',
@@ -244,10 +246,26 @@ export default {
 		}
 	},
 	methods: {
-		downloadYearCanvas: function(){
-			let canvas = document.getElementById('preview')
-			var pdf = new jsPDF();
+		downloadYearCanvas: function(idx){
+			let canvasid;
+			let option;
+			if(idx == 0){
+				canvasid = "preview";
+				option = {}
+			}else{
+				canvasid = "desk";
+				option = {
+					orientation: "landscape",
+					format:[129,180]
+				}
+			}
+
+			let canvas = document.getElementById(canvasid)
+
+			
+			var pdf = new jsPDF(option);
 			var width = pdf.internal.pageSize.width;
+			console.log(width)
 
 			var date = new Date(this.picker)
 			if (canvas.getContext) {
@@ -256,15 +274,15 @@ export default {
 
 			//ノートのページを追加
 			context.fillStyle = "rgb(255,255,255)";
-			context.fillRect(0,0,canvasWidth,canvasHeight);
-			context.drawImage(this.noteImg, 0, 0, canvasWidth, canvasHeight);
+			context.fillRect(0,0,canvasWidth[idx],canvasHeight[idx]);
+			context.drawImage(this.noteImg, 0, 0, canvasWidth[idx], canvasHeight[idx]);
 			var imgData = canvas.toDataURL("image/jpeg");
 			pdf.addImage(imgData, 'JPEG', 0, 0,width,0);
 
 			for(var i = 0; i < 12; i++){
 				pdf.addPage();
 				var tmpDate = new Date(date.getFullYear()+"-"+ String(i+1))
-				this.DrawTemplate(context,tmpDate);
+				this.DrawTemplate(context,tmpDate,idx);
 
 				var imgData = canvas.toDataURL("image/jpeg");
 				pdf.addImage(imgData, 'JPEG', 0, 0,width,0);
@@ -291,15 +309,15 @@ export default {
 			
 			if (canvas.getContext) {
 				var context = canvas.getContext('2d');
-				this.DrawTemplate(context,new Date(this.picker));
+				this.DrawTemplate(context,new Date(this.picker),0);
 			}
 		},
-		DrawTemplate: function(cont,current){
+		DrawTemplate: function(cont,current,idx){
 			
 			var year = current.getFullYear();
 			var month = current.getMonth() + 1;
 			
-			var tmp_x = (FRAME_RD.x - FRAME_LD.x)/7;
+			var tmp_x = (FRAME_RD[idx].x - FRAME_LD[idx].x)/7;
 			
 			cont.fillStyle = "rgb(255,255,255)";
 			cont.fillRect(0,0,890 * scale,840 * scale);
@@ -310,22 +328,22 @@ export default {
 			var fontsize = 90  * scale;
 			cont.font = `${fontsize}px "YuMincho"`;
 			if(monthfontsLength == 2){
-				cont.fillText( month.toString(), MONTH_POS.x - 40 * scale, MONTH_POS.y);
+				cont.fillText( month.toString(), MONTH_POS[idx].x - MONTH_OFFSET[idx] * scale, MONTH_POS[idx].y);
 			}else{
-				cont.fillText( month.toString(), MONTH_POS.x, MONTH_POS.y);
+				cont.fillText( month.toString(), MONTH_POS[idx].x, MONTH_POS[idx].y);
 			}
 			
 			fontsize = 30 * scale;
 			cont.font = `${fontsize}px 'YuMincho'`;
-			cont.fillText( year.toString(), YEAR_POS.x, YEAR_POS.y);
+			cont.fillText( year.toString(), YEAR_POS[idx].x, YEAR_POS[idx].y);
 			
 			fontsize = 25 * scale;
 			cont.font = `${fontsize}px 'YuMincho'`;
 			var monthtextfontsLength = MONTH_EN[month-1].length;
-			cont.fillText( MONTH_EN[month-1], MONTH_EN_POS.x - (monthtextfontsLength * 8 * scale), MONTH_EN_POS.y);
+			cont.fillText( MONTH_EN[month-1], MONTH_EN_POS[idx].x - (monthtextfontsLength * MONTH_EN_OFFSET[idx] * scale), MONTH_EN_POS[idx].y);
 							
-			cont.drawImage(this.topImgs[month-1], PIC_POS.x, PIC_POS.y,205 * scale,154 * scale);
-			cont.drawImage(this.listImg, HYOU_POS.x, HYOU_POS.y,200 * scale,100 * scale);
+			cont.drawImage(this.topImgs[month-1], PIC_POS[idx].x, PIC_POS[idx].y,205 * scale,154 * scale);
+			cont.drawImage(this.listImg, HYOU_POS[idx].x, HYOU_POS[idx].y,200 * scale,100 * scale);
 			
 			var weekdayData = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
@@ -333,10 +351,10 @@ export default {
 			cont.fillStyle = "#000";
 			cont.beginPath();
 			//パスの開始座標を指定する
-			cont.moveTo(FRAME_LU.x,FRAME_LU.y);
-			cont.lineTo(FRAME_LU.x,FRAME_LU.y + WEEKFRAME_OFFSET.y);
-			cont.lineTo(FRAME_RU.x,FRAME_LU.y + WEEKFRAME_OFFSET.y);
-			cont.lineTo(FRAME_RU.x,FRAME_LU.y);
+			cont.moveTo(FRAME_LU[idx].x,FRAME_LU[idx].y);
+			cont.lineTo(FRAME_LU[idx].x,FRAME_LU[idx].y + WEEKFRAME_OFFSET[idx].y);
+			cont.lineTo(FRAME_RU[idx].x,FRAME_LU[idx].y + WEEKFRAME_OFFSET[idx].y);
+			cont.lineTo(FRAME_RU[idx].x,FRAME_LU[idx].y);
 			//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
 			cont.closePath();
 			//現在のパスを輪郭表示する
@@ -346,14 +364,14 @@ export default {
 			for (var i = 0; i < weekdayData.length; i++) {
 				//曜日の背景色を描画
 				cont.fillStyle = colorList[i];
-				cont.fillRect(FRAME_LU.x + i * tmp_x + 1, FRAME_LU.y + WEEKFRAME_OFFSET.y + 1, tmp_x-2, -WEEKFRAME_OFFSET.y-2)
+				cont.fillRect(FRAME_LU[idx].x + i * tmp_x + 1, FRAME_LU[idx].y + WEEKFRAME_OFFSET[idx].y + 1, tmp_x-2, -WEEKFRAME_OFFSET[idx].y-2)
 
 				//曜日の外枠を描画
 				cont.fillStyle = "#000";
 				cont.beginPath();
 				//パスの開始座標を指定する
-				cont.moveTo(FRAME_LU.x + i * tmp_x, FRAME_LU.y);
-				cont.lineTo(FRAME_LU.x + i * tmp_x, FRAME_LU.y + WEEKFRAME_OFFSET.y);
+				cont.moveTo(FRAME_LU[idx].x + i * tmp_x, FRAME_LU[idx].y);
+				cont.lineTo(FRAME_LU[idx].x + i * tmp_x, FRAME_LU[idx].y + WEEKFRAME_OFFSET[idx].y);
 				//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
 				cont.closePath();
 				//現在のパスを輪郭表示する
@@ -369,7 +387,7 @@ export default {
 				cont.fillStyle = "#FFF";
 				fontsize = 23 * scale;
 				cont.font = `${fontsize}px 'Hiragino Maru Gothic ProN'`;
-				cont.fillText(weekdayData[i], FRAME_LU.x + WEEK_OFFSET.x + i * tmp_x, FRAME_LU.y + WEEK_OFFSET.y);
+				cont.fillText(weekdayData[i], FRAME_LU[idx].x + WEEK_OFFSET[idx].x + i * tmp_x, FRAME_LU[idx].y + WEEK_OFFSET[idx].y);
 			}
 			
 			var Cdata= get_month_calendar(year,month);
@@ -389,11 +407,11 @@ export default {
 			//外枠を描画
 			cont.beginPath();
 			//パスの開始座標を指定する
-			cont.moveTo(FRAME_LU.x,FRAME_LU.y);
+			cont.moveTo(FRAME_LU[idx].x,FRAME_LU[idx].y);
 			//座標を指定してラインを引いていく
-			cont.lineTo(FRAME_LD.x,FRAME_LD.y);
-			cont.lineTo(FRAME_RD.x,FRAME_RD.y);
-			cont.lineTo(FRAME_RU.x,FRAME_RU.y);
+			cont.lineTo(FRAME_LD[idx].x,FRAME_LD[idx].y);
+			cont.lineTo(FRAME_RD[idx].x,FRAME_RD[idx].y);
+			cont.lineTo(FRAME_RU[idx].x,FRAME_RU[idx].y);
 			//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
 			cont.closePath();
 			//現在のパスを輪郭表示する
@@ -402,11 +420,11 @@ export default {
 			//縦線を描画
 			cont.beginPath();
 			var tmp_vec = new Vector2(tmp_x,0);
-			var line_pos = new Vector2(FRAME_LU.x,FRAME_LU.y);
+			var line_pos = new Vector2(FRAME_LU[idx].x,FRAME_LU[idx].y);
 			line_pos.add(tmp_vec);
 			for(var i = 0; i < 6; i++){
 				cont.moveTo(line_pos.x,line_pos.y);
-				cont.lineTo(line_pos.x,FRAME_LD.y);
+				cont.lineTo(line_pos.x,FRAME_LD[idx].y);
 				line_pos.add(tmp_vec);
 			}
 			//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
@@ -417,13 +435,13 @@ export default {
 			
 			//横線を引いていく
 			cont.beginPath();
-			var tmp_y = (FRAME_LD.y - FRAME_LU.y)/y_count;
+			var tmp_y = (FRAME_LD[idx].y - FRAME_LU[idx].y)/y_count;
 			tmp_vec = new Vector2(0,tmp_y);
-			line_pos = new Vector2(FRAME_LU.x,FRAME_LU.y);
+			line_pos = new Vector2(FRAME_LU[idx].x,FRAME_LU[idx].y);
 			line_pos.add(tmp_vec);
 			for(var i = 0; i < 6; i++){
 				cont.moveTo(line_pos.x,line_pos.y);
-				cont.lineTo(FRAME_RD.x,line_pos.y);
+				cont.lineTo(FRAME_RD[idx].x,line_pos.y);
 				line_pos.add(tmp_vec);
 			}
 			//パスを閉じる（最後の座標から開始座標に向けてラインを引く）
@@ -435,7 +453,7 @@ export default {
 			var y_carsor = 0;
 			
 			for(var i = 0; i < Cdata.length; i++){
-				var lu_pos = new Vector2(FRAME_LU.x + Cdata[i]['weekday'] * tmp_x, FRAME_LU.y + tmp_y * y_carsor)
+				var lu_pos = new Vector2(FRAME_LU[idx].x + Cdata[i]['weekday'] * tmp_x, FRAME_LU[idx].y + tmp_y * y_carsor)
 				
 				//日付の描画
 				if(Cdata[i]['weekday'] == 0){
@@ -447,14 +465,14 @@ export default {
 				}
 				fontsize = 21 * scale
 				cont.font = `${fontsize}px 'YuMincho'`;
-				cont.fillText(Cdata[i]['day'], lu_pos.x + DAY_OFFSET.x, lu_pos.y + DAY_OFFSET.y);
+				cont.fillText(Cdata[i]['day'], lu_pos.x + DAY_OFFSET[idx].x, lu_pos.y + DAY_OFFSET[idx].y);
 				
 				//干支の描画
 				cont.fillStyle = "#000";
 				fontsize = 13 * scale
 				cont.font = `${fontsize}px 'Hiragino Maru Gothic ProN'`;
 				var kanshi = get_kanshi(new Date(year,month-1,Cdata[i]['day']));
-				cont.fillText(kanshi, lu_pos.x + KANSHI_OFFSET.x, lu_pos.y + KANSHI_OFFSET.y);
+				cont.fillText(kanshi, lu_pos.x + KANSHI_OFFSET[idx].x, lu_pos.y + KANSHI_OFFSET[idx].y);
 				
 				cont.fillStyle = "#000";
 				fontsize = 11 * scale
@@ -462,30 +480,30 @@ export default {
 				var unsei = get_unsei(new Date(year,month-1,Cdata[i]['day']), this.selectPatern);
 				//console.log(UNSEI[unsei].length)
 				if(UNSEI[unsei].length == 2){
-					cont.fillText(UNSEI[unsei], lu_pos.x + UNSEI_OFFSET.x + 6 * scale, lu_pos.y + UNSEI_OFFSET.y);
+					cont.fillText(UNSEI[unsei], lu_pos.x + UNSEI_OFFSET[idx].x + 6 * scale, lu_pos.y + UNSEI_OFFSET[idx].y);
 				}else{
-					cont.fillText(UNSEI[unsei], lu_pos.x + UNSEI_OFFSET.x, lu_pos.y + UNSEI_OFFSET.y);
+					cont.fillText(UNSEI[unsei], lu_pos.x + UNSEI_OFFSET[idx].x, lu_pos.y + UNSEI_OFFSET[idx].y);
 				}
 				
 				if(unsei == 0 || unsei == 1 || unsei == 2){
 					//ハナマル
-					cont.drawImage(this.hanaImg, lu_pos.x+ICON_OFFSET.x, lu_pos.y+ICON_OFFSET.y,25 * scale,25 * scale);
+					cont.drawImage(this.hanaImg, lu_pos.x+ICON_OFFSET[idx].x, lu_pos.y+ICON_OFFSET[idx].y,25 * scale,25 * scale);
 				}else if(unsei == 3 || unsei == 4 || unsei == 5){
 					//天中殺
-					cont.drawImage(this.tentyusatsuImg, lu_pos.x+ICON_OFFSET.x-9 * scale, lu_pos.y+ICON_OFFSET.y + 3,45 * scale,22 * scale);
+					cont.drawImage(this.tentyusatsuImg, lu_pos.x+ICON_OFFSET[idx].x-9 * scale, lu_pos.y+ICON_OFFSET[idx].y + 3,45 * scale,22 * scale);
 				}else if(unsei == 6 || unsei == 7 || unsei == 8){
 					//マル
-					cont.drawImage(this.maruImg, lu_pos.x+ICON_OFFSET.x, lu_pos.y+ICON_OFFSET.y,20 * scale,20 * scale);
+					cont.drawImage(this.maruImg, lu_pos.x+ICON_OFFSET[idx].x, lu_pos.y+ICON_OFFSET[idx].y,20 * scale,20 * scale);
 				}else if (unsei == 9){
 					//バツ
-					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET.x+5 * scale, lu_pos.y+ICON_OFFSET.y+7 * scale,13 * scale,13 * scale);
+					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET[idx].x+5 * scale, lu_pos.y+ICON_OFFSET[idx].y+7 * scale,13 * scale,13 * scale);
 				}else if(unsei == 10){
 					//二重丸
-					cont.drawImage(this.nijuImg, lu_pos.x+ICON_OFFSET.x, lu_pos.y+ICON_OFFSET.y,20 * scale,20 * scale);
+					cont.drawImage(this.nijuImg, lu_pos.x+ICON_OFFSET[idx].x, lu_pos.y+ICON_OFFSET[idx].y,20 * scale,20 * scale);
 				}else if(unsei == 11){
 					//バツバツ
-					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET.x, lu_pos.y+ICON_OFFSET.y+7 * scale,13 * scale,13 * scale);
-					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET.x+13 * scale, lu_pos.y+ICON_OFFSET.y+7 * scale,13 * scale,13 * scale);
+					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET[idx].x, lu_pos.y+ICON_OFFSET[idx].y+7 * scale,13 * scale,13 * scale);
+					cont.drawImage(this.batsuImg, lu_pos.x+ICON_OFFSET[idx].x+13 * scale, lu_pos.y+ICON_OFFSET[idx].y+7 * scale,13 * scale,13 * scale);
 				}
 				
 				if(Cdata[i]['weekday'] >= 6){
